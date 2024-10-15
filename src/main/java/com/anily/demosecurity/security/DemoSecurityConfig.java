81e2f2474727f1c1a732dfef047a82fffeb79321
anilyildiz <anil-yildiz@outlook.com.tr>
2024-10-15 07:45:14 +0300

    JDBC authentication implemented

--- src/main/java/com/anily/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/anily/demosecurity/security/DemoSecurityConfig.java
@@ -4,35 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails tyrande = User.builder()
-                .username("tyrande")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails illidan = User.builder()
-                .username("illidan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails malfurion = User.builder()
-                .username("malfurion")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(tyrande, illidan, malfurion);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
@@ -58,3 +41,28 @@ public class DemoSecurityConfig {
     }
 
 }
+
+/*
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails tyrande = User.builder()
+                .username("tyrande")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails illidan = User.builder()
+                .username("illidan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails malfurion = User.builder()
+                .username("malfurion")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(tyrande, illidan, malfurion);
+    }
+    */
